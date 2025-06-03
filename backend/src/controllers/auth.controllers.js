9c607c13082984782f054e18f0bb5644d1970a75
dkhobragade <dikshantkhobragade12@gmail.com>
2025-06-03 14:32:06 +0530

    feat: add proxy-from-env package and implement proxy handling
    - Added proxy-from-env package to manage proxy settings based on environment variables.
    - Created index.js for proxy logic and package.json for package metadata.
    - Implemented comprehensive test suite in test.js to validate proxy behavior under various scenarios.
    - Updated package-lock.json to include new dependencies.
    - Added axios as a dependency for making HTTP requests.
    - Enhanced logout functionality in auth.controllers.js to clear JWT cookie and return appropriate response.
    - Integrated cookie-parser middleware in index.js to handle cookies in requests.
    - Created auth.middleware.js to protect routes by verifying JWT from cookies and ensuring user existence.

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -102,5 +102,16 @@ export const login = async (req, res) => {
 }
 
 export const logout = (req, res) => {
-    res.send("singup Page");
+    try {
+        res.cookie("jwt", "", {maxAge:0})
+        res.status(200).json({
+            message: "User logged out successfully"
+        });
+    } catch (error) {
+
+    console.log("Error in logout",error.message)
+        res.status(500).json({
+            message: 'Internal Server Error'
+        });
+    }
 }
---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,44 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/user.model.js';
+
+
+export const protectRoute = async (req, res, next) => {
+
+    try {
+
+        const token = req.cookies.jwt
+
+        if (!token) {
+            return res.status(401).json({
+                message: "Unauthorized access, please login"
+            })
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        if (!decoded) {
+            return res.status(401).json({
+                message: "Invalid token, please login again"
+            });
+        }
+
+        const user = await User.findById(decoded.id).select("-password");
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        req.user = user;
+        next();
+
+    } catch (error) {
+
+        console.error("Error in authMiddleware:", error.message);
+        return res.status(500).json({
+            message: "Internal Server Error"
+        });
+
+    }
+}
